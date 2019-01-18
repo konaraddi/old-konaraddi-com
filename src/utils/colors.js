3a953bab5229257317e7a902f084a2254ab74f47
konaraddio <omkar.konaraddi@gmail.com>
2019-01-18 11:24:18 -0500

    Change background color from Neutral White 4 to Warm White 1

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,6 @@
 // https://www.ibm.com/design/language/resources/color-library/
 export default {
   primary: "#fed500", // yellow 10
-  white: "#f3f3f3", // neutral white 4
+  white: "#fdfcfc", // warm white 1
   black: "#272727" // gray 90
 };
